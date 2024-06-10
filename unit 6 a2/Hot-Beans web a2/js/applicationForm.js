a323f938be10c8fb53a6fdb66656ab865affdc08
galm333 <145364869+galm333@users.noreply.github.com>
2024-06-10 15:58:10 +0100

    Delete unit 6 a2 directory

--- unit 6 a2/Hot-Beans web a2/js/applicationForm.js
+++
@@ -1,6 +0,0 @@
-const select = document.querySelector('select');
-
-//select "Applied Position" automatically based on the fragment (the value after "#")
-window.addEventListener("load", function() {
-    select.selectedIndex = window.location.hash.toString().substring(1);
-});
